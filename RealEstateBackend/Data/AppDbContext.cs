5078a6b886b478e87ee2d0f7e8c060692d8041eb
Shahd Abdalla <110117862+Shahdabdalla123@users.noreply.github.com>
2025-04-07 18:24:20 +0200

    Added OrderDetails

--- RealEstateBackend/Data/AppDbContext.cs
+++ RealEstateBackend/Data/AppDbContext.cs
@@ -6,9 +6,14 @@ namespace RealEstate.Data
 {
     public class AppDbContext : IdentityDbContext<Account>
     {
+        public virtual DbSet<Order> Orders {  get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<OrderItem> OrderItems { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Wishlist> Wishlists { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            
+                        
         }
     }
 }
---
+++ RealEstateBackend/Models/Domains/Product.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using RealEstate.Models.Attributes;
+
+namespace RealEstate.Models.Domains
+{
+    public class Product
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Length(1, 50)]
+        [Required]
+        public string Name { get; set; }
+
+        [Length(1, 200)]
+        [Required]
+        public string? Description { get; set; }
+
+        [NonNegative]
+        [Column(TypeName = "decimal(18,2)")]
+        [Required]
+        public decimal Price { get; set; }
+
+        [NonNegative]
+        [Required]
+        public int Quantity { get; set; }
+        public bool IsUsed { get; set; } = false;
+
+        public bool IsDeleted { get; set; } = false;
+
+        [ForeignKey("Category")]
+        public int? CategoryID { get; set; }
+        public virtual Category? Category { get; set; }
+        public virtual ICollection<OrderItem>? OrderItems { get; set; }
+        public virtual ICollection<Wishlist>? Wishlist { get; set; }
+
+
+
+    }
+
+}
