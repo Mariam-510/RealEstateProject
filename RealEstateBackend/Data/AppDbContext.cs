de81abd6e3b150c3e16bf21a59782f7b1c6c22b7
Yusuf Emam <yusufemam@hotmail.com>
2025-04-07 18:54:09 +0200

    updates

--- RealEstateBackend/Data/AppDbContext.cs
+++ RealEstateBackend/Data/AppDbContext.cs
@@ -6,13 +6,29 @@ namespace RealEstate.Data
 {
     public class AppDbContext : IdentityDbContext<Account>
     {
+<<<<<<< Updated upstream
+=======
+        #region DBSETS
+        public virtual DbSet<Admin> Admins { get; set; }
+        public virtual DbSet<Agent> Agents { get; set; }
+        public virtual DbSet<Buyer> Buyers { get; set; }
+        public virtual DbSet<Seller> Sellers { get; set; }
+        public virtual DbSet<Contract> Contracts { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<OrderItem> OrderItems { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Wishlist> Wishlists { get; set; }
+        public virtual DbSet<Message> Messages { get; set; }
+        public virtual DbSet<Conversation> Conversations { get; set; }
+        public virtual DbSet<PropertyBid> PropertyBids { get; set; } 
+        #endregion
+
+>>>>>>> Stashed changes
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             
         }
 
-        public virtual DbSet<Message> Messages { get; set; }
-        public virtual DbSet<Conversation> Conversations { get; set; }
-        public virtual DbSet<PropertyBid> PropertyBids { get; set; }
     }
 }
--- RealEstateBackend/Models/Domains/Account.cs
+++ RealEstateBackend/Models/Domains/Account.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace RealEstate.Models.Domains
 {
     public class Account : IdentityUser
     {
+<<<<<<< Updated upstream
 
+=======
+        [DataType(DataType.DateTime)]
+        public DateTime CteatedAt { get; set; } = DateTime.Now;
+>>>>>>> Stashed changes
     }
 }
