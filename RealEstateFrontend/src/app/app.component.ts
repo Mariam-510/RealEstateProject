5d96d14bd8d4bb7760a4afb310d422fcf195e4ce
Ebrahim Abdelkarim <ebrahimabdelkarim57@gmail.com>
2025-04-11 18:48:42 +0200

    initial commit

--- RealEstateFrontend/src/app/app.component.ts
+++ RealEstateFrontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CategoryComponent } from "./Components/category/category.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CategoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
