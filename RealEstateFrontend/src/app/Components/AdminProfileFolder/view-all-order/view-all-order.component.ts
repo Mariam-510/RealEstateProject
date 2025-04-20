73b0805cfbf37a18ff911d508e7f4d128a25010f
Shahd Abdalla <110117862+Shahdabdalla123@users.noreply.github.com>
2025-04-20 17:22:46 +0200

    firstCommit

---
+++ RealEstateFrontend/src/app/Components/AdminProfileFolder/view-all-order/view-all-order.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-order',
+  imports: [],
+  templateUrl: './view-all-order.component.html',
+  styleUrl: './view-all-order.component.css'
+})
+export class ViewAllOrderComponent {
+
+}
