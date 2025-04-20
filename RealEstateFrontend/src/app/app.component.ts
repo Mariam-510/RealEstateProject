1cb5198c0ca883aaefd4f637d76bf22a4f7e7946
Shahd Abdalla <110117862+Shahdabdalla123@users.noreply.github.com>
2025-04-20 18:26:30 +0200

    Revert "SeconCommit"
    This reverts commit c2997dc791eac20e34658003afda6b25045a63d1.

--- RealEstateFrontend/src/app/app.component.ts
+++ RealEstateFrontend/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterModule,CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
