2f02473431cf83564ccf3fa8473d7b64e59e2afb
Ebrahim Abdelkarim <ebrahimabdelkarim57@gmail.com>
2025-04-26 20:52:18 +0300

    Add category component frontend/backend connection

--- RealEstateFrontend/src/app/Services/ApiServices/category.service.ts
+++ RealEstateFrontend/src/app/Services/ApiServices/category.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../../app.config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  private apiUrl = `${API_CONFIG.apiUrl}api/Category`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createCategory(categoryData: FormData): Observable<any> {
+    return this.http.post(`${this.apiUrl}/CreateCategory`, categoryData);
+  }
 }
