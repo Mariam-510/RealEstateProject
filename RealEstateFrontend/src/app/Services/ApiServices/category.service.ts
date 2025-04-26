f82861d12e421d3c67b48344b6bde61006bb7b20
Ebrahim Abdelkarim <ebrahimabdelkarim57@gmail.com>
2025-04-26 21:31:13 +0300

    Fix category after merge conflicts

--- RealEstateFrontend/src/app/Services/ApiServices/category.service.ts
+++ RealEstateFrontend/src/app/Services/ApiServices/category.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
 import { Observable } from 'rxjs';
 import { API_CONFIG } from '../../app.config';
 
@@ -9,35 +8,26 @@ export interface CategoryDTOShow {
   name: string;
   categoryimage: string;
 }
-=======
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { API_CONFIG } from '../../app.config';
->>>>>>> IbrahimFront7
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryService {
   private apiUrl = `${API_CONFIG.apiUrl}api/Category`;
-<<<<<<< HEAD
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // New method to get all categories
-  getAllCategories(): Observable<{ message: string, categoryDto: CategoryDTOShow[] }> {
-    return this.http.get<{ message: string, categoryDto: CategoryDTOShow[] }>(
+  getAllCategories(): Observable<{
+    message: string;
+    categoryDto: CategoryDTOShow[];
+  }> {
+    return this.http.get<{ message: string; categoryDto: CategoryDTOShow[] }>(
       `${this.apiUrl}/GetAllCategory`
     );
   }
 
-
-=======
-
-  constructor(private http: HttpClient) {}
-
   createCategory(categoryData: FormData): Observable<any> {
     return this.http.post(`${this.apiUrl}/CreateCategory`, categoryData);
   }
->>>>>>> IbrahimFront7
 }
