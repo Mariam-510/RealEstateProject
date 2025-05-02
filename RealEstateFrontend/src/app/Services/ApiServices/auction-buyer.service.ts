2255dceeef653ef33419cba7997006fb776bc6f5
Mariam Ashraf <mariamashrafamin@gmail.com>
2025-05-03 01:02:58 +0300

    add auction buyer back - service

--- RealEstateFrontend/src/app/Services/ApiServices/auction-buyer.service.ts
+++ RealEstateFrontend/src/app/Services/ApiServices/auction-buyer.service.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // Correct import
 import { API_CONFIG } from '../../app.config';
-import { HttpClient } from '@microsoft/signalr';
+import { Observable } from 'rxjs';
+
+// Add your DTO interfaces
+interface CreateAuctionBuyerDto {
+  auctionId: number;
+  paymentId: number;
+}
+
+interface AuctionBuyerDto {
+  id: number;
+  date: Date;
+  isDeleted: boolean;
+  buyerId?: number;
+  auctionId?: number;
+  paymentId?: number;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +26,13 @@ export class AuctionBuyerService {
 
   constructor(private http: HttpClient) { }
 
+  // Get Auction Buyer by Auction ID
+  getByAuctionAndBuyerId(auctionId: number): Observable<AuctionBuyerDto> {
+    return this.http.get<AuctionBuyerDto>(`${this.apiUrl}/AuctionBuyer/${auctionId}`);
+  }
+
+  // Create new Auction Buyer
+  createAuctionBuyer(createDto: CreateAuctionBuyerDto): Observable<{ message: string, auctionBuyerDto: AuctionBuyerDto }> {
+    return this.http.post<{ message: string, auctionBuyerDto: AuctionBuyerDto }>(this.apiUrl, createDto);
+  }
 }
