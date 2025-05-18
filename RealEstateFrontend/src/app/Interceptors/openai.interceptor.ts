f6f2468934f9acaba79776a202671d605c01a4a3
Yusuf Emam <yusufemam@hotmail.com>
2025-05-18 20:52:46 +0300

    fix v3

--- RealEstateFrontend/src/app/Interceptors/openai.interceptor.ts
+++ RealEstateFrontend/src/app/Interceptors/openai.interceptor.ts
@@ -14,7 +14,7 @@ export class OpenaiInterceptor implements HttpInterceptor {
     if (request.url === environment.openaiUrl) {
       const cloned = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${environment.openaiKey}`,
+          Authorization: `Bearer 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`,
           'Content-Type': 'application/json'
         }
       });
