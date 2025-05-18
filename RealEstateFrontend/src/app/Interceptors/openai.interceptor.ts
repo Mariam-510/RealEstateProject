24f5d3178eb2bd6d211e8bbdfc6f39b7c1a9c56a
Yusuf Emam <yusufemam@hotmail.com>
2025-05-18 21:37:22 +0300

    ya mosahel

--- RealEstateFrontend/src/app/Interceptors/openai.interceptor.ts
+++ RealEstateFrontend/src/app/Interceptors/openai.interceptor.ts
@@ -14,7 +14,7 @@ export class OpenaiInterceptor implements HttpInterceptor {
     if (request.url === environment.openaiUrl) {
       const cloned = request.clone({
         setHeaders: {
-          Authorization: `Bearer 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`,
+          Authorization: `Bearer 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`,
           'Content-Type': 'application/json'
         }
       });
